a33eb8f52afc6cbf25df9d9bcd49675f521af3de
nadeemk2023 <nadeemekhan@gmail.com>
2025-06-17 14:59:54 -0700

    refactor: update handleSearch function to utilize useRouter for navigation

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,6 +6,7 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
@@ -15,6 +16,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
