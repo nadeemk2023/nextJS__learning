a608266734d023349a832a1cf39c4e1f70386cd5
nadeemk2023 <nadeemekhan@gmail.com>
2025-06-22 17:04:10 -0700

    feat: add error handling component and improve error logging in data fetching functions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,7 +61,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  //   throw new Error("Failed to Delete Invoice");
+  throw new Error("Failed to Delete Invoice");
   await sql`
     DELETE FROm invoices WHERE id = ${id}
     `;
